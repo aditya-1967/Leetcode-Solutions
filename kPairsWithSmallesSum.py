01b15fd7db3a3905bee1a8dea2b56c4c4e9a829a
Aditya Aman <72183883+aditya-1967@users.noreply.github.com>
2022-10-16 10:06:05 +0530

    Leetcode 373

---
+++ kPairsWithSmallesSum.py
@@ -0,0 +1,20 @@
+# 373. Find K Pairs with Smallest Sums
+# You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+# Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+# Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        queue = []
+        def push(i, j):
+            if i < len(nums1) and j < len(nums2):
+                heapq.heappush(queue, [nums1[i] + nums2[j], i, j])
+        push(0, 0)
+        pairs = []
+        while queue and len(pairs) < k:
+            _, i, j = heapq.heappop(queue)
+            pairs.append([nums1[i], nums2[j]])
+            push(i, j + 1)
+            if j == 0:
+                push(i + 1, 0)
+        return pairs
