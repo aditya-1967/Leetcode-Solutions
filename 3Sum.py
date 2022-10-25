39ff9b614a8ece38500b0e0027fd086ad75152db
Aditya Aman <72183883+aditya-1967@users.noreply.github.com>
2022-10-25 09:21:47 +0530

    Leetcode 15

---
+++ 3Sum.py
@@ -0,0 +1,27 @@
+# 15. 3Sum
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+# Notice that the solution set must not contain duplicate triplets.
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        nums.sort()
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i-1]:
+                continue
+            left, right = i+1, len(nums)-1
+            while left < right:
+                sum = a + nums[left] + nums[right]
+                if sum > 0:
+                    right -= 1
+                elif sum < 0:
+                    left += 1
+                else:
+                    ans.append([a, nums[left], nums[right]])
+                    left += 1
+                    while nums[left] == nums[left-1] and left < right:
+                        left += 1
+                        
+                        
+            
+        return ans
