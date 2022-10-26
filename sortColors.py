f211ff99cca2c80749dee382128e18cc76d930e2
Aditya Aman <72183883+aditya-1967@users.noreply.github.com>
2022-10-26 10:05:47 +0530

    Leetcode 75

---
+++ sortColors.py
@@ -0,0 +1,17 @@
+# 75. Sort Colors
+# Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+# We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+# You must solve this problem without using the library's sort function.
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            for j in range(len(nums)-1):
+                if nums[j+1] < nums[j]:
+                    temp = nums[j+1]
+                    nums[j+1] = nums[j]
+                    nums[j] = temp
+        
